388b333d133fe768fbcec1147e20177455e23523
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2024-01-23 19:57:55 +0200

    test

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,10 +1,30 @@
-import { HomePageSection } from "./HomePageStyled";
+import {
+  HomePageImg,
+  HomePageLi,
+  HomePageSection,
+  HomePageUl,
+  HomePageDivUl,
+  HomePageImgBacgraund,
+} from "./HomePageStyled";
+import imgCarsHome from "../../../imgCarsHome.json";
+import homeBackgraund from "../image/Home/HomeCarBackgraund.png";
 
 const HomePage = () => {
   return (
     <HomePageSection>
-      <div></div>
-      <div></div>
+      <HomePageImgBacgraund src={homeBackgraund} />
+      <div>
+        <h1>Car Car Car</h1>
+      </div>
+      <HomePageDivUl>
+        <HomePageUl>
+          {imgCarsHome.map((car, index) => (
+            <HomePageLi key={index}>
+              <HomePageImg src={car.img} alt={`Car ${index + 1}`} />
+            </HomePageLi>
+          ))}
+        </HomePageUl>
+      </HomePageDivUl>
     </HomePageSection>
   );
 };
--- src/components/pages/HomePageStyled.js
+++ src/components/pages/HomePageStyled.js
@@ -1,4 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 export const HomePageSection = styled.section`
-  width: 100%;
+  display: flex;
+`;
+
+export const HomePageImgBacgraund = styled.img`
+  position: absolute;
+  right: 0;
+`;
+
+const moveAnimation = keyframes`
+  0% {
+    margin-left: 0;
+  }
+  100% {
+    margin-left: -1200px; 
+  }
+`;
+
+export const HomePageLi = styled.li`
+  flex: 0 0 auto;
+  margin-right: 5px;
+`;
+
+export const HomePageImg = styled.img`
+  min-width: 500px;
+  max-height: 500px;
+  border-radius: 5px;
+  object-fit: cover;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const HomePageUl = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  width: 2000px;
+  overflow: hidden;
+  animation: ${moveAnimation} 10s linear infinite;
+`;
+
+export const HomePageDivUl = styled.div`
+  width: 600px;
+  overflow: hidden;
+  border-radius: 60px;
 `;
